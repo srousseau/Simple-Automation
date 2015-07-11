b3cb7703bc9ea35d8536be3303282acc61211340
Scott Rousseau <scott.rousseau@gmail.com>
2015-07-11 13:43:22 -0700

    Edit comments

--- MouseNow.py
+++ MouseNow.py
@@ -7,10 +7,9 @@ print "Press Ctrl-C to quit."
 
 try:
     while True:
-        # TODO: Get and print the mouse coordinates
+        # Get and print the mouse coordinates
         x, y = pyautogui.position()
         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
         print positionStr
-        #print "\b" * len(positionStr), 
 except KeyboardInterrupt:
     print "\nDone"
